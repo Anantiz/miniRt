5dd910cd1e7b2548297d3907294127c876961f95
Loris <loris.kary67730@gmqil.com>
2024-02-21 09:13:49 +0100

    add struct

--- includes/miniRt.h
+++ includes/miniRt.h
@@ -6,14 +6,14 @@
 /*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 00:24:18 by aurban            #+#    #+#             */
-/*   Updated: 2024/02/19 13:51:08 by loris            ###   ########.fr       */
+/*   Updated: 2024/02/20 16:46:21 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include <math.h>
+# include <math.h>
 
 # include "../libs/MLX42/include/MLX42/MLX42_Int.h"
 # include "libft.h"
@@ -24,6 +24,15 @@ typedef	struct s_vector
 	float	y;
 	float	z;
 }           t_vector;
+
+typedef	struct	s_camera
+{
+	t_vector	pos;
+	t_vector	direction;
+	t_vector	right;
+	t_vector	up;
+	double		fov;
+}				t_camera;
 
 /* VECTOR OPERATOR */
 
