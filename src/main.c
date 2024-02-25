8a1ad74d040e2cdf316e5d2d1a31a19a434476c8
aurban <antoine.urban.68@gmail.com>
2024-02-25 08:40:59 +0100

    Parsing, good progress

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 01:01:39 by aurban            #+#    #+#             */
-/*   Updated: 2024/02/22 20:07:04 by aurban           ###   ########.fr       */
+/*   Updated: 2024/02/25 08:40:57 by aurban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 #define SMALL_WIN_X	640
 #define SMALL_WIN_Y	480
 
-static void	init_global(t_glob *glob)
-{
-	glob->win_size = (t_pair_size){SMALL_WIN_X, SMALL_WIN_Y};
-	glob->mlx = mlx_init(glob->win_size.width, glob->win_size.height, \
-	"RTT", false);
-	if (!glob->mlx)
-		error_exit("Failed to initialize MLX");
-	glob->img = mlx_new_image(glob->mlx, glob->win_size.width , \
-	glob->win_size.height);
-	if (!glob->img)
-	{
-		mlx_terminate(glob->mlx);
-		error_exit("Failed to initialize MLX image");
-	}
-	mlx_key_hook(glob->mlx, cptn_hook_key, glob);
-}
+// static void	init_global(t_glob *glob)
+// {
+// 	glob->win_size = (t_pair_size){SMALL_WIN_X, SMALL_WIN_Y};
+// 	glob->mlx = mlx_init(glob->win_size.width, glob->win_size.height, \
+// 	"RTT", false);
+// 	if (!glob->mlx)
+// 		error_exit("Failed to initialize MLX");
+// 	glob->img = mlx_new_image(glob->mlx, glob->win_size.width , \
+// 	glob->win_size.height);
+// 	if (!glob->img)
+// 	{
+// 		mlx_terminate(glob->mlx);
+// 		error_exit("Failed to initialize MLX image");
+// 	}
+// 	mlx_key_hook(glob->mlx, cptn_hook_key, glob);
+// }
 
 int main(int argc, char **argv)
 {
@@ -48,10 +48,10 @@ int main(int argc, char **argv)
 	if (parse_map(&glob, argv[1]))
 		return (FAILURE);
 	printf("\033[32mParsing done\033[0m\n");
-	init_global(&glob);
-	mlx_loop_hook(glob.mlx, rtt_render, &glob);
-	mlx_loop(glob.mlx);
-	mlx_terminate(glob.mlx);
+	// init_global(&glob);
+	// mlx_loop_hook(glob.mlx, rtt_render, &glob);
+	// mlx_loop(glob.mlx);
+	// mlx_terminate(glob.mlx);
 	safe_as_fuck_malloc(0, NULL, SAFE_MALLOC_FREE_ALL);
 	return (0);
 }
