73c0912507975eb99fb752f53895344bf6f5a43c
Loris Kary-Potes <lkary-po@k2r3p1.42mulhouse.fr>
2024-03-01 11:32:51 +0100

    shading

--- src/Reindeer/RTT_render.c
+++ src/Reindeer/RTT_render.c
@@ -24,18 +24,22 @@ void	rtt_render_pixel(t_glob *glob, t_collision *collision, int x, int y, t_ray 
 		t_lcol *light_col;
 		light_col = query_visible_light(collision->obj, &collision->point, ray->direction);
 		vector_normalizer(&(collision->point));
-		mlx_put_pixel(glob->img, x, y, vector_to_color(&collision->point, light_adjust(light_col)));
+		mlx_put_pixel(glob->img, x, y, vector_to_color(&collision->point, light_adjust(light_col, collision)));
 		fprintf(debug_log_f, "\t~Collision\n");
 	}
 }
 
-float	light_adjust(t_lcol *light_col)
+float	light_adjust(t_lcol *light_col, t_collision *col)
 {
 	float		cos_angle;
+	t_vector	*obj_center = add_vector(&col->obj->l->pos, &col->parent_obj->pos);
 
 	if (light_col)
 	{
-		cos_angle = cos(light_col->theta);
+		t_vector	*norm;
+		norm = sub_vector(&col->point, obj_center);
+		cos_angle = vec_dot_product((&light_col->light->pos), norm);
+		// cos_angle = cos(cos_angle);
 		if (cos_angle < 0)
 			cos_angle = 0;
 		fprintf(debug_log_f, "ca c'est ma fusee encule de ta mere la pute %f\n", cos_angle);
