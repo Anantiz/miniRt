d5998a7e79feeb0ae400f5e8bb6c59eeb8fc98dd
aurban <antoine.urban.68@gmail.com>
2024-03-13 10:52:40 +0100

    Plane norm sucks

--- src/Reindeer/RTT_render.c
+++ src/Reindeer/RTT_render.c
@@ -26,7 +26,8 @@ void	rtt_render_pixel(t_glob *glob, t_collision *collision, int x, int y, t_ray 
 	light_col = query_visible_light(collision->obj, &collision->point, ray->dir);
 	if (!light_col) // Means the point is in the shadow
 	{
-		mlx_put_pixel(glob->img, x, y, vector_to_color(0.3, &collision->obj->l->rgb));
+		// We do this for now because we don't have ambient light
+		mlx_put_pixel(glob->img, x, y, vector_to_color(0.25, &collision->obj->l->rgb));
 		return ;
 	}
 	mlx_put_pixel(glob->img, x, y, vector_to_color(light_col->cos_angle, &collision->obj->l->rgb));
